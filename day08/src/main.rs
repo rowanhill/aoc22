d91df14bac3c90f30afacadac5e833c98d0b64c9
Rowan Hill <rowan.hill@gmail.com>
2022-12-08 22:13:30 +0000

    Day 08 refactor

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -15,82 +15,55 @@ fn parse_grid<const N: usize>(path: &str) -> Grid<N> {
     grid
 }
 
+// NOTE: Original solution didn't include this function, but instead basically copied the body 4 times,
+// once for each direction
+fn scan_grid<const N: usize>(
+    grid: &mut Grid<N>,
+    outer: &(impl Iterator<Item = usize> + Clone),
+    inner: &(impl Iterator<Item = usize> + Clone),
+    is_row: bool,
+    dir_index: usize,
+) {
+    for i in outer.clone() {
+        let mut tallest = -1i8;
+        let mut dists_to_not_smaller = [0; 10];
+        for j in inner.clone() {
+            // Are we iterating over a row or a column?
+            let (x, y) = if is_row { (j, i) } else { (i, j) };
+
+            // Check if the tree is visible from the edge in this direction
+            if grid[y][x].0 > tallest {
+                grid[y][x].1 = true;
+                tallest = grid[y][x].0;
+            }
+
+            // Record the distance to the nearest tree that is >= the height of this tree in this direction
+            grid[y][x].2[dir_index] = dists_to_not_smaller[(grid[y][x].0 as usize)];
+
+            // Distance for trees this height or smaller reset to 1 (this is the closest tree >= their height)
+            for h in 0..=grid[y][x].0 {
+                dists_to_not_smaller[h as usize] = 1;
+            }
+            // Distance for taller trees increments by one (this is another tree they can see over)
+            for h in (grid[y][x].0+1)..=9 {
+                dists_to_not_smaller[h as usize] += 1;
+            }
+        }
+    }
+}
+
 fn main() {
     // const GRID_SIZE: usize = 5;
     // let mut grid = parse_grid::<GRID_SIZE>("example.txt");
     const GRID_SIZE: usize = 99;
     let mut grid = parse_grid::<GRID_SIZE>("input.txt");
 
-    for y in 0..GRID_SIZE {
-        // Left to right
-        let mut tallest = -1i8;
-        let mut height_counts = [0; 10];
-        for x in 0..GRID_SIZE {
-            if grid[y][x].0 > tallest {
-                grid[y][x].1 = true;
-                tallest = grid[y][x].0;
-            }
-            grid[y][x].2[0] = height_counts[(grid[y][x].0 as usize)];
-            for h in 0..=grid[y][x].0 {
-                height_counts[h as usize] = 1;
-            }
-            for h in (grid[y][x].0+1)..=9 {
-                height_counts[h as usize] += 1;
-            }
-        }
-
-        // Right to left
-        tallest = -1;
-        height_counts = [0; 10];
-        for x in (0..GRID_SIZE).rev() {
-            if grid[y][x].0 > tallest {
-                grid[y][x].1 = true;
-                tallest = grid[y][x].0;
-            }
-            grid[y][x].2[1] = height_counts[(grid[y][x].0 as usize)];
-            for h in 0..=grid[y][x].0 {
-                height_counts[h as usize] = 1;
-            }
-            for h in (grid[y][x].0+1)..=9 {
-                height_counts[h as usize] += 1;
-            }
-        }
-    }
-    for x in 0..GRID_SIZE {
-        // Top to bottom
-        let mut tallest = -1;
-        let mut height_counts = [0; 10];
-        for y in 0..GRID_SIZE {
-            if grid[y][x].0 > tallest {
-                grid[y][x].1 = true;
-                tallest = grid[y][x].0;
-            }
-            grid[y][x].2[2] = height_counts[(grid[y][x].0 as usize)];
-            for h in 0..=grid[y][x].0 {
-                height_counts[h as usize] = 1;
-            }
-            for h in (grid[y][x].0+1)..=9 {
-                height_counts[h as usize] += 1;
-            }
-        }
-
-        // Bottom to top
-        tallest = -1;
-        height_counts = [0; 10];
-        for y in (0..GRID_SIZE).rev() {
-            if grid[y][x].0 > tallest {
-                grid[y][x].1 = true;
-                tallest = grid[y][x].0;
-            }
-            grid[y][x].2[3] = height_counts[(grid[y][x].0 as usize)];
-            for h in 0..=grid[y][x].0 {
-                height_counts[h as usize] = 1;
-            }
-            for h in (grid[y][x].0+1)..=9 {
-                height_counts[h as usize] += 1;
-            }
-        }
-    }
+    let range = 0..GRID_SIZE;
+    let revrs = range.clone().rev();
+    scan_grid::<GRID_SIZE>(&mut grid, &range, &range, true, 0);
+    scan_grid::<GRID_SIZE>(&mut grid, &range, &revrs, true, 1);
+    scan_grid::<GRID_SIZE>(&mut grid, &revrs, &range, false, 2);
+    scan_grid::<GRID_SIZE>(&mut grid, &revrs, &revrs, false, 3);
 
     let num_visible = grid.iter()
         .flat_map(|row| row.iter().map(|(_, v, _)| v))
