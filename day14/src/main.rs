5687a887ccc2f5503248b7af6e7ce8e502009594
Rowan Hill <rowan.hill@gmail.com>
2022-12-14 21:09:44 +0000

    Day 14 stack solution

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -13,17 +13,14 @@ fn main() {
     let mut map = HashMap::new();
     let mut max_y = 0;
     for line in input.lines() {
-        // println!("{line}");
         let mut coord_iter = line.split(" -> ")
             .map(|s| s.split_once(",").unwrap())
             .map(|(x, y)| (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap()));
         let mut prev = coord_iter.next().unwrap();
         max_y = max(max_y, prev.1);
         while let Some(cur) = coord_iter.next() {
-            // println!("({printl:?}) to ({:?})", prev, cur);
             for x in min(prev.0, cur.0)..=max(prev.0, cur.0) {
                 for y in min(prev.1, cur.1)..=max(prev.1, cur.1) {
-                    // println!("({x},{y})");
                     map.insert((x, y), Rock);
                 }
             }
@@ -32,47 +29,34 @@ fn main() {
         }
     }
 
-    let mut sand_pos = (500, 0);
+    let mut pt1_printed = false;
+    let mut sand_count = 0;
+    let mut path = vec![];
+    path.push((500, 0));
     loop {
-        match (
-            map.contains_key(&(sand_pos.0 - 1, sand_pos.1 + 1)),
-            map.contains_key(&(sand_pos.0, sand_pos.1 + 1)),
-            map.contains_key(&(sand_pos.0 + 1, sand_pos.1 + 1))
+        let sand_pos = path.last().unwrap();
+        let new_pos = match (
+            map.contains_key(&(sand_pos.0 - 1, sand_pos.1 + 1)) || sand_pos.1 >= max_y + 1,
+            map.contains_key(&(sand_pos.0, sand_pos.1 + 1)) || sand_pos.1 >= max_y + 1,
+            map.contains_key(&(sand_pos.0 + 1, sand_pos.1 + 1)) || sand_pos.1 >= max_y + 1
         ) {
-            (_, false, _) => { sand_pos = (sand_pos.0, sand_pos.1 + 1); },
-            (false, _, _) => { sand_pos = (sand_pos.0 - 1, sand_pos.1 + 1); },
-            (_, _, false) => { sand_pos = (sand_pos.0 + 1, sand_pos.1 + 1); },
-            (true, true, true) => { map.insert(sand_pos, Sand); sand_pos = (500, 0); }
+            (_, false, _) => Some((sand_pos.0, sand_pos.1 + 1)),
+            (false, _, _) => Some((sand_pos.0 - 1, sand_pos.1 + 1)),
+            (_, _, false) => Some((sand_pos.0 + 1, sand_pos.1 + 1)),
+            (true, true, true) => { map.insert(sand_pos.clone(), Sand); sand_count += 1; None }
+        };
+        if let Some(p) = new_pos {
+            path.push(p);
+            if !pt1_printed && p.1 > max_y {
+                println!("Part 1: {sand_count}");
+                pt1_printed = true;
+            }
+        } else {
+            path.pop();
         }
-        if sand_pos.1 > max_y {
+        if path.is_empty() {
             break;
         }
     }
-    let num_sands = map.values().filter(|s| match s { Sand => true, Rock => false }).count();
-    println!("Part 1: {num_sands}");
-
-    sand_pos = (500, 0);
-    loop {
-        match (
-            map.contains_key(&(sand_pos.0 - 1, sand_pos.1 + 1)),
-            map.contains_key(&(sand_pos.0, sand_pos.1 + 1)),
-            map.contains_key(&(sand_pos.0 + 1, sand_pos.1 + 1))
-        ) {
-            (_, false, _) => { sand_pos = (sand_pos.0, sand_pos.1 + 1); },
-            (false, _, _) => { sand_pos = (sand_pos.0 - 1, sand_pos.1 + 1); },
-            (_, _, false) => { sand_pos = (sand_pos.0 + 1, sand_pos.1 + 1); },
-            (true, true, true) => {
-                map.insert(sand_pos, Sand);
-                if sand_pos == (500, 0) {
-                    break;
-                }
-                sand_pos = (500, 0); }
-        }
-        if sand_pos.1 == max_y + 1 {
-            map.insert(sand_pos, Sand);
-            sand_pos = (500, 0);
-        }
-    }
-    let num_sands = map.values().filter(|s| match s { Sand => true, Rock => false }).count();
-    println!("Part 2: {num_sands}");
+    println!("Part 2: {sand_count}");
 }
