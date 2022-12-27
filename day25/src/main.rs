201a93f9fd2b3b381188f4714445524a22ce5131
Rowan Hill <rowan.hill@gmail.com>
2022-12-27 12:16:11 +0000

    Day 25 improved approach (div/mod)

--- day25/src/main.rs
+++ day25/src/main.rs
@@ -11,36 +11,49 @@ fn snafu_char_to_decimal(snafu: &u8) -> isize {
 
 fn snafu_to_decimal(snafu: &str) -> isize {
     let mut result = 0isize;
-    for (i, c) in snafu.as_bytes().iter().rev().enumerate() {
-        result += (5 as usize).pow(i as u32) as isize * snafu_char_to_decimal(c);
+    let mut col_val = 1;
+    for c in snafu.as_bytes().iter().rev() {
+        result += col_val * snafu_char_to_decimal(c);
+        col_val *= 5;
     }
     result
 
 }
 
+/*
+Encoding in a standard base is a simple case of div and mod. SNAFU is a *balanced* quinary system,
+however, where digits can represent a negative value in a column. This complicates things slightly,
+because encoding a single value between 0 and 4 (inclusive) can involve two columns:
+  0 => 00
+  1 => 01
+  2 => 02
+  3 => 1=
+  4 => 1-
+This means that, as we encode each digit, we need to track whether or not we're "carrying" one from
+encoding the digit in the column to the right.
+ */
 fn decimal_to_snafu(decimal: isize) -> String {
-    let mut result = String::from("2");
-    let mut result_decimal = snafu_to_decimal(&result);
-    while decimal > result_decimal {
-        result.push('2');
-        result_decimal = snafu_to_decimal(&result);
+    let mut snafu_digits = vec![];
+
+    let mut num = decimal;
+    let mut carry = 0;
+
+    while num > 0 || carry > 0 {
+        let digit_plus_carry = num % 5 + carry;
+        num = num / 5;
+        let c = match digit_plus_carry % 5 {
+            0 => '0',
+            1 => '1',
+            2 => '2',
+            3 => '=',
+            4 => '-',
+            _ => unreachable!()
+        };
+        snafu_digits.push(c);
+        carry = if digit_plus_carry > 2 { 1 } else { 0 };
     }
 
-    'outer: for n in 0..result.len() {
-        for (cur, next) in [("2", "1"), ("1", "0"), ("0","-"), ("-", "=")] {
-            if result_decimal > decimal && &result[n..=n] == cur {
-                result.replace_range(n..=n, next);
-                let new_decimal = snafu_to_decimal(&result);
-                if new_decimal < decimal {
-                    result.replace_range(n..=n, cur);
-                    continue 'outer;
-                }
-                result_decimal = new_decimal;
-            }
-        }
-    }
-
-    result
+    snafu_digits.iter().rev().collect()
 }
 
 fn main() {
@@ -48,4 +61,18 @@ fn main() {
     let part1_decimal = input.lines().map(|line| snafu_to_decimal(line)).sum::<isize>();
     assert_eq!(part1_decimal, snafu_to_decimal(&decimal_to_snafu(part1_decimal)));
     println!("Part 1: {}", decimal_to_snafu(part1_decimal));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn ints_to_snafu_and_back() {
+        for i in 1..100 {
+            let snafu = decimal_to_snafu(i);
+            let decimal = snafu_to_decimal(&snafu);
+            assert_eq!(i, decimal, "{} -> {} -> {}", i, snafu, decimal);
+        }
+    }
 }
